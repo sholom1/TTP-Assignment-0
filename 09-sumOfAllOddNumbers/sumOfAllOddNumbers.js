321b27b1f3cc0924c5b37bdc42b8663deec35b55
Sam Kessler <sholomkessler1@gmail.com>
2020-12-19 21:39:41 -0500

    Completed test-09

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,9 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+	let count = 0;
+	nums.forEach((element) => {
+		if (element % 2 != 0) count++;
+	});
+	return count;
 }
 
 // Do not edit this line;
