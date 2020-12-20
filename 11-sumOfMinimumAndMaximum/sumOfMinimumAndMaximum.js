8e377126cce303c7c4e427de4a226a345c8a6338
Sam Kessler <sholomkessler1@gmail.com>
2020-12-19 21:46:20 -0500

    Completed test-10

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,11 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+	let min = Number.MAX_VALUE;
+	let max = Number.MIN_VALUE;
+	nums.forEach((element) => {
+		min = Math.min(element, min);
+		max = Math.max(element, max);
+	});
+	return min + max;
 }
 
 // Do not edit this line;
