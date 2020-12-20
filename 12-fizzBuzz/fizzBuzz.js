2e7f839341776a6aeb6aa2a408bcc882e4512d2e
Sam Kessler <sholomkessler1@gmail.com>
2020-12-19 22:00:15 -0500

    Completed test-11

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,13 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+	let output = [];
+	for (let value = start; value <= end; value++) {
+		let returnString = '';
+		if (value % 3 == 0) returnString += 'Fizz';
+		if (value % 5 == 0) returnString += 'Buzz';
+		if (returnString.length > 0) output.push(returnString);
+		else output.push(value);
+	}
+	return output;
 }
 
 // Do not edit this line;
