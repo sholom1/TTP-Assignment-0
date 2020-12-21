7e30b6a268c1b458ed41a54cdce00d88a57466f6
Sam Kessler <sholomkessler1@gmail.com>
2020-12-20 23:52:44 -0500

    Completed test 19. Assignment finished.

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -1,11 +1,17 @@
 class MySolution {
-  constructor() {
-    this.flag = false; // this is optional to use;
-  }
+	constructor() {
+		this.flag = false; // this is optional to use;
+	}
 
-  binarySearch(nums, target) {
-    // Insert code here;
-  }
+	binarySearch(nums, target) {
+		let mid = Math.floor(nums.length / 2);
+		if (target == nums[mid]) return true;
+		else if (nums.length > 1) {
+			if (target > nums[mid]) return this.binarySearch(nums.slice(mid), target);
+			else if (target < nums[mid]) return this.binarySearch(nums.slice(0, mid), target);
+		}
+		return false;
+	}
 }
 
 // Do not edit this line;
