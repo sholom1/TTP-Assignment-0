7e3f2894faa1d2514c7f2098fb1a6359666429a8
Sam Kessler <sholomkessler1@gmail.com>
2020-12-20 22:58:58 -0500

    Completed test 17

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,11 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+	let result = new Set();
+	for (const set of args) {
+		for (const value of set) {
+			result.add(value);
+		}
+	}
+	return result;
 }
 
 // Do not edit this line;
